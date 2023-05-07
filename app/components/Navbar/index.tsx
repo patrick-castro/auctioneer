e4425eb77ca1696c3c956bbc18b51bb16bf8df41
patrick-castro <castropatrickantonio@gmail.com>
2023-05-08 03:44:22 +0800

    Add index page

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -6,10 +6,10 @@ import { FcDonate } from 'react-icons/fc'
 
 export default function Navbar() {
   return (
-    <nav className='h-20 px-8 sm:px-24 flex items-center fixed w-full justify-between'>
+    <nav className='h-20 px-8 sm:px-24 flex items-center fixed w-full justify-between z-10'>
       <Link href='/' className='flex items-center gap-2'>
         <FcDonate className='text-3xl' />
-        <h1 className='text-2xl font-semibold'>Auction</h1>
+        <h1 className='text-2xl font-semibold'>Auctioneer</h1>
       </Link>
       <Dropdown />
     </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-between mx-8 sm:mx-24'>
-      Main Page
+    <main>
+      <div className='w-full'>
+        <Image
+          src='/images/add-cart.jpeg'
+          alt='Add to cart image'
+          priority={true}
+          fill={true}
+          className='image-brightness-wrapper'
+        />
+
+        <div className='z-10 absolute top-80 left-20 gap-20'>
+          <h1 className='text-5xl font-semibold pb-2 text-white text-shadow'>
+            Winning is just a bid away.
+          </h1>
+
+          <p className='text-2xl pl-1 font-normal pb-2 text-white text-shadow-sm'>
+            Experience the thrill of Auctioneer.
+          </p>
+          <div className='flex gap-5 mt-4'>
+            <button className='w-40 py-5 bg-indigo-500 hover:bg-indigo-600 rounded-lg font-semibold text-white shadow-lg'>
+              Login
+            </button>
+            <button className='w-40 py-5 bg-white outline outline-2 outline-indigo-500 rounded-lg font-bold text-indigo-600 hover:text-white hover:bg-indigo-500 shadow-lg'>
+              Register
+            </button>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
