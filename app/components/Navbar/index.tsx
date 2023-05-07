f9bf4a74d2422b3b9f1512ad325aca9fc970ae25
patrick-castro <castropatrickantonio@gmail.com>
2023-05-08 00:46:30 +0800

    Add icons

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -2,11 +2,13 @@
 
 import Link from 'next/link'
 import { Dropdown } from '../Dropdown'
+import { FcDonate } from 'react-icons/fc'
 
 export default function Navbar() {
   return (
     <nav className='h-20 px-8 sm:px-24 flex items-center fixed w-full justify-between'>
-      <Link href='/'>
+      <Link href='/' className='flex items-center gap-2'>
+        <FcDonate className='text-3xl' />
         <h1 className='text-2xl font-semibold'>Auction</h1>
       </Link>
       <Dropdown />
