9e64eb030371eb5d4e1cc8769a1a4d639ff8325b
patrick-castro <castropatrickantonio@gmail.com>
2023-05-08 19:53:33 +0800

    Add Login and Register forms

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,51 @@
+import Link from 'next/link'
+
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <div className='login-form'>
+      <div className='pb-4'>
+        <h1 className='text-2xl font-semibold mb-2'>Login to Auctioneer</h1>
+        <p>Winning is just a bid away!</p>
+      </div>
+      <form>
+        <div className='mt-5'>
+          <p className='text-sm font-semibold mb-2 text-gray-500'>
+            Email Address<span className='text-red-500 text-sm'>*</span>
+          </p>
+          <input
+            type='email'
+            className='container mt-0 mb-2 h-14 py-4 box-border px-5 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500'
+            name='email'
+          />
+        </div>
+
+        <div className='mt-5'>
+          <p className='text-sm font-semibold mb-2 text-gray-500'>
+            Password<span className='text-red-500 text-sm'>*</span>
+          </p>
+          <input
+            type='password'
+            className='container mt-0 mb-2 h-14 py-4 box-border px-5 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500'
+            name='password'
+          />
+        </div>
+
+        <button
+          className='btn btn-primary w-full mt-8 text-white bg-blue-500 hover:bg-blue-600 rounded-md py-4'
+          type='submit'
+        >
+          Log in
+        </button>
+      </form>
+
+      <div>
+        <p className='text-center pt-8'>
+          Not on Auctioneer yet?{' '}
+          <Link href='/register' className='text-blue-700'>
+            Register now!
+          </Link>
+        </p>
+      </div>
+    </div>
+  )
 }
