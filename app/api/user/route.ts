ee75c6009a0d31aaa884af76feb53ec0af1551bd
patrick-castro <castropatrickantonio@gmail.com>
2023-05-09 18:59:56 +0800

    Add function for creating user

---
+++ app/api/user/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+import prisma from '@/lib/prisma'
+import bcrypt from 'bcrypt'
+
+interface RequestBody {
+  email: string
+  password: string
+}
+
+export async function POST(request: Request) {
+  const body: RequestBody = await request.json()
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: await bcrypt.hash(body.password, 10),
+    },
+  })
+
+  const { password, ...result } = user
+
+  return new Response(JSON.stringify(result))
+}
---
+++ lib/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client'
+
+const globalforPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma = globalforPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalforPrisma.prisma = prisma
+
+export default prisma
