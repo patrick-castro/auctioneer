6933bfd59c9fbbb030a7575badf265f48f28fe2d
patrick-castro <castropatrickantonio@gmail.com>
2023-05-10 20:36:17 +0800

    Add endpoint for updating user

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,5 +1,7 @@
+import { verifyJwt } from '@/lib/jwt'
 import prisma from '@/lib/prisma'
 import bcrypt from 'bcrypt'
+import { NextResponse } from 'next/server'
 
 interface RequestBody {
   email: string
@@ -18,4 +20,37 @@ export async function POST(request: Request) {
   const { password, ...result } = user
 
   return new Response(JSON.stringify(result))
+}
+
+export async function PUT(request: Request) {
+  const accessToken = request.headers.get('authorization') || ''
+  const jwt = verifyJwt(accessToken)
+
+  if (!accessToken || !jwt) {
+    return new NextResponse(
+      JSON.stringify({
+        error: 'unauthorized',
+      }),
+      {
+        status: 401,
+      }
+    )
+  }
+
+  const body: { balance: string } = await request.json()
+  const { id: userId, balance } = jwt
+  const totalBalance = parseFloat(balance) + parseFloat(body.balance)
+
+  await prisma.user.update({
+    data: {
+      balance: totalBalance,
+    },
+    where: {
+      id: userId,
+    },
+  })
+
+  return new NextResponse(
+    JSON.stringify({ message: 'successfully updated user' })
+  )
 }
