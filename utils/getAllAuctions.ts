ba5bd2039636f3d4c635d7a5941ddb47f8629d67
patrick-castro <castropatrickantonio@gmail.com>
2023-05-14 04:27:05 +0800

    Delete unused util

--- utils/getAllAuctions.ts
+++
@@ -1,9 +0,0 @@
-const appUrl = process.env.APP_URL
-
-export default async function getAllAuctions(searchFilter: string) {
-  const res = await fetch(`${appUrl}/api/auction`)
-
-  if (!res.ok) throw new Error('Failed to fetch data')
-
-  return res.json()
-}
