bfb5b2a6ea19bc36cd1fd8362aaeca327817e5fe
patrick-castro <castropatrickantonio@gmail.com>
2023-05-14 03:43:03 +0800

    Add conditional rendering to the buttons at Home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,44 @@ import { useSession } from 'next-auth/react'
 import { RiAuctionLine } from 'react-icons/ri'
 
 export default function Home() {
-  const { data: session } = useSession()
+  const { status } = useSession()
+
+  const renderButtons = () => {
+    if (status === 'loading') return
+
+    if (status === 'unauthenticated') {
+      return (
+        <>
+          <div className='flex gap-5 mt-4'>
+            <Link
+              href='/login'
+              className='w-40 py-5 text-center bg-indigo-500 hover:bg-indigo-600 rounded-lg font-semibold text-white shadow-lg'
+            >
+              Login
+            </Link>
+            <Link
+              href='/register'
+              className='w-40 py-5 text-center bg-white outline outline-2 outline-indigo-500 rounded-lg font-bold text-indigo-600 hover:text-white hover:bg-indigo-500 shadow-lg'
+            >
+              Register
+            </Link>
+          </div>
+        </>
+      )
+    }
+
+    return (
+      <div className='flex gap-5 mt-4'>
+        <Link
+          href='/auction'
+          className='w-72 py-5 text-center bg-indigo-500 hover:bg-indigo-600 rounded-lg font-semibold text-white shadow-lg flex items-center justify-center gap-2'
+        >
+          Let&apos;s Start Auctioning!
+          <RiAuctionLine className='text-xl' />
+        </Link>
+      </div>
+    )
+  }
 
   return (
     <main>
@@ -26,34 +63,7 @@ export default function Home() {
           <p className='text-2xl pl-1 font-normal pb-2 text-white text-shadow-sm'>
             Experience the thrill of Auctioneer.
           </p>
-          {!session || !session.user ? (
-            <>
-              <div className='flex gap-5 mt-4'>
-                <Link
-                  href='/login'
-                  className='w-40 py-5 text-center bg-indigo-500 hover:bg-indigo-600 rounded-lg font-semibold text-white shadow-lg'
-                >
-                  Login
-                </Link>
-                <Link
-                  href='/register'
-                  className='w-40 py-5 text-center bg-white outline outline-2 outline-indigo-500 rounded-lg font-bold text-indigo-600 hover:text-white hover:bg-indigo-500 shadow-lg'
-                >
-                  Register
-                </Link>
-              </div>
-            </>
-          ) : (
-            <div className='flex gap-5 mt-4'>
-              <Link
-                href='/auction'
-                className='w-72 py-5 text-center bg-indigo-500 hover:bg-indigo-600 rounded-lg font-semibold text-white shadow-lg flex items-center justify-center gap-2'
-              >
-                Let&apos;s Start Auctioning!
-                <RiAuctionLine className='text-xl' />
-              </Link>
-            </div>
-          )}
+          {renderButtons()}
         </div>
       </div>
     </main>
